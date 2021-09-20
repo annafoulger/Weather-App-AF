b9c25781b8e18c7712e5002ea5469643d3ccea49
annafoulger <annafoulger@gmail.com>
2021-09-19 18:30:46 -0700

    Incorporating API

---
+++ src/app.js
@@ -0,0 +1,18 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+}
+let apiKey = "ea85eed299f838f08577ab2943f15dd1";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
